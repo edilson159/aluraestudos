f8b7b430a4ab8ec6861e6e613f910bfd9ec0830a
edilson159 <146896391+edilson159@users.noreply.github.com>
2023-11-03 11:40:33 -0300

    Atuakizei o repositorio

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,17 @@
-alert('Olá mundo');
+function tocaSom (idElementoAudio) {
+   document.querySelector(idElementoAudio).play();
+}
+ 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0 ;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+   listaDeTeclas[contador].onclick= tocaSom ;
+
+   contador = contador + 1;
+
+   console.log(contador);
+
+}
