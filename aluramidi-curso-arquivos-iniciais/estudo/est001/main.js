141573a611be1da8c3a511a6fca9b950e22bd364
edilson159 <146896391+edilson159@users.noreply.github.com>
2023-11-06 20:32:22 -0300

    atualizei o repositorio

--- aluramidi-curso-arquivos-iniciais/estudo/est001/main.js
+++ aluramidi-curso-arquivos-iniciais/estudo/est001/main.js
@@ -8,18 +8,9 @@ let contador = 0 ;
 
 //enquanto
 while (contador < listaDeTeclas.length) {
-
-   const tecla = listaDeTeclas[contador]
-
-  const instrumento = tecla.classList[1] ;
-
-const idAudio = `#som_${instrumento}`;
-
-   console.log(idAudio);
-
-   tecla.onclick = function () {
-      tocaSom(idAudio);
-   } 
+   listaDeTeclas[contador].onclick= function () {
+      tocaSom('#som_tecla_pom');
+   } ;
 
    contador = contador + 1;
 
