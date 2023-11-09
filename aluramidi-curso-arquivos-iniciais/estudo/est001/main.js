564bd7b728cdb1b8639c5d9acf7fbe021d673bad
edilson159 <146896391+edilson159@users.noreply.github.com>
2023-11-09 14:46:18 -0300

    updat

--- aluramidi-curso-arquivos-iniciais/estudo/est001/main.js
+++ aluramidi-curso-arquivos-iniciais/estudo/est001/main.js
@@ -1,28 +1,26 @@
-function tocaSom (idElementoAudio) {
-   document.querySelector(idElementoAudio).play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
- 
-const listaDeTeclas = document.querySelectorAll('.tecla');
 
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+console.log(listaDeTeclas);
 //enquanto
-for ( let contador = 0; contador < listaDeTeclas.length; contador++) {
-   
-   const tecla = listaDeTeclas[contador];
-   const instrumento = tecla.classList[1] ;
-   const idAudio = `#som_${instrumento}`;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+  console.log(instrumento);
 
-   
-      tecla.onclick = function () {
-      tocaSom('#som_tecla_pom');
-      console.log(idAudio);
-   } ;
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+    console.log(idAudio);
+  };
+  tecla.onkeydown = function () {
+    tecla.classList.add("ativa");
+  };
 
-   tecla.onkeydown = function (){
-      tecla.classList.add('ativa');
-   }
-
-   tecla.onkeyup = function () {
-      tecla.classList.remove('ativa');
-   }
-
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
