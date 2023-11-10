9d21163f57b24c40cf4da2762d77a7768a2266e0
edilson159 <146896391+edilson159@users.noreply.github.com>
2023-11-10 11:02:47 -0300

    update the repository

--- aluramidi-curso-arquivos-iniciais/estudo/est001/main.js
+++ aluramidi-curso-arquivos-iniciais/estudo/est001/main.js
@@ -2,6 +2,13 @@ function tocaSom(idElementoAudio) {
   document.querySelector(idElementoAudio).play();
 }
 
+if (elemento && elemento.localName === 'audio') {
+  elemento.play()
+}
+else {
+  console.log('Elemento não encontrado ou seletor inválido');
+}
+
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 //enquanto
@@ -9,15 +16,12 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
-  console.log(instrumento);
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
   tecla.onkeydown = function (evento) {
-    console.log(evento.code == "Space");
-
-    if (evento.code == "Space") {
+    if (evento.code == "Space" || evento.code === "Enter") {
       tecla.classList.add("ativa");
     }
   };
