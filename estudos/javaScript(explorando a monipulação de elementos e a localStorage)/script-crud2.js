a1cd72a656bb08bcc2874a6b59a617bab71321b0
edilson159 <146896391+edilson159@users.noreply.github.com>
2023-12-01 00:05:23 -0300

    update

--- estudos/javaScript(explorando a monipulação de elementos e a localStorage)/script-crud2.js
+++ estudos/javaScript(explorando a monipulação de elementos e a localStorage)/script-crud2.js
@@ -8,7 +8,7 @@ const removeButon = document.querySelector(".app__form-footer__button--cancel");
 
 const ulTasks = document.querySelector(".app__section-task-list");
 
-const tasks = JSON.parse[localStorage.getItem("tasks")] || [];
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function creatTask(task) {
   const li = document.createElement("li");
@@ -16,23 +16,25 @@ function creatTask(task) {
 
   const svg = document.createElement("svg");
   svg.innerHTML = `
-    <svg>
         <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
 
     `;
-  const Paragraph = document.createElement("p");
-  Paragraph.textContent = task.descricao;
-  const button = document.createElement("button");
-  const imgButton = document.createElement("img");
+  const paragraph = document.createElement("p");
+  paragraph.textContent = task.descricao;
+  paragraph.classList.add('app__section-task-list-item-description')
 
+  const button = document.createElement("button");
+  button.classList.add('app_button-edit')
+
+  const imgButton = document.createElement("img");
   imgButton.setAttribute("src", "./imagens/edit.png");
   button.append(imgButton);
 
   li.append(svg);
-  li.append(Paragraph);
+  li.append(paragraph);
   li.append(button);
 
   return li;
@@ -53,15 +55,17 @@ function onSubmit(formAddTask) {
       concluida: false,
     };
     tasks.push(task);
+    const elementTask = creatTask(task)
+    ulTasks.append(elementTask)
     localStorage.setItem("tasks", JSON.stringify(tasks));
     textArea.value = "";
+    formAddTask.classList.add('hidden')
   });
 }
 onSubmit(formAddTask, removeButon);
 
 function removeForm(removeButon, formAddTask) {
   removeButon.addEventListener("click", (evento) => {
-    evento.preventDefault();
     formAddTask.classList.add("hidden");
     textArea.value = "";
   });
@@ -69,7 +73,14 @@ function removeForm(removeButon, formAddTask) {
 
 removeForm(removeButon, formAddTask);
 
-tasks.forEach((task) => {
-  const elementTask = createElementTask(task);
+function traverseArray (creatTask) {
+  tasks.forEach((task) => {
+  console.log(task)
+  const elementTask = creatTask(task);
   ulTasks.append(elementTask);
+  
 });
+}
+
+traverseArray(creatTask)
+
