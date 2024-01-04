4425a618a719b5f9426c55a4a767185b9c627af2
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-01-04 10:59:43 -0300

    update

--- estudos/javaScript(explorando a monipulação de elementos e a localStorage)/script-crud2.js
+++ estudos/javaScript(explorando a monipulação de elementos e a localStorage)/script-crud2.js
@@ -1,8 +1,40 @@
 const addTaskBtn = document.querySelector(".app__button--add-task");
 const formAddTask = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
+const ulTasks =  document.querySelector('.app__section-task-list')
 
-const tasks = []
+const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+
+function createElementTasks(task) {
+  const li = document.createElement('li')
+  li.classList.add('app__section-task-list-item')
+
+  const svg = document.createElement('svg')
+  svg.innerHTML = `
+  <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+  `
+
+  const paraghaph = document.createElement('p')
+  paraghaph.classList.add("app__section-task-list-item-description")
+  paraghaph.textContent = task.description
+
+  const button = document.createElement('button')
+  button.classList.add('app_button-edit')
+  const imageButton = document.createElement('img')
+  
+  imageButton.setAttribute('src', 'imagens/edit.png')
+  button.append(imageButton)
+
+  li.append(svg)
+  li.append(paraghaph)
+  li.append(button)
+  
+  return li
+}
 
 addTaskBtn.addEventListener("click", () => {
   formAddTask.classList.toggle("hidden");
@@ -13,6 +45,13 @@ formAddTask.addEventListener("submit", (event) => {
   const task = {
     description : textArea.value
   }
-  tasks.push(task)
-  localStorage.setItem('tasks',tasks)
+  textArea.value = ''
+  formAddTask.classList.add('hidden')
+   tasks.push(task)
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+});
+
+tasks.forEach(task => {
+  const elementTask = createElementTasks(task)
+  ulTasks.append(elementTask)
 });
