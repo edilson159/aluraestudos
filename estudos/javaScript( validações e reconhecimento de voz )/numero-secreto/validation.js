5616bf24a6314162ab2b9484e8d57bc7a9d42c8a
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-01-05 22:18:07 -0300

    upgate

--- estudos/javaScript( validações e reconhecimento de voz )/numero-secreto/validation.js
+++ estudos/javaScript( validações e reconhecimento de voz )/numero-secreto/validation.js
@@ -3,23 +3,24 @@ function rightKick(kick) {
 
   if (kickForInvalid(number)) {
     displayingNumber.innerHTML += "<div>valor invalido</div>";
-    return
+    return;
   }
 
   if (numberIsHightOrthanAllowed(number)) {
     displayingNumber.innerHTML += `<div>valor invalido: Fale um número entre ${lowerValue} e ${highestValue}</div>`;
-    return
+    return;
   }
 
   if (number == numberSecret) {
     document.body.innerHTML = `
     <h2>Você acertou !</h2>
     <h3>O número secreto era ${numberSecret}</h3>
+    <button id='jogar novamente' class='btn-jogar'>Jogar novamente </button>
     `;
   } else if (number > numberSecret) {
-    displayingNumber.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    displayingNumber.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
   } else {
-    displayingNumber.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    displayingNumber.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
   }
 }
 function kickForInvalid(number) {
@@ -29,3 +30,9 @@ function kickForInvalid(number) {
 function numberIsHightOrthanAllowed(number) {
   return number > highestValue || number < lowerValue;
 }
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar novament") {
+    window.location.reload();
+  }
+});
