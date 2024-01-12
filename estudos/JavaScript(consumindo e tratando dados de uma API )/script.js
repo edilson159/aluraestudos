de915474c20bbae333e38436f3bd360788ae73e4
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-01-12 10:03:17 -0300

    update

--- estudos/JavaScript(consumindo e tratando dados de uma API )/script.js
+++ estudos/JavaScript(consumindo e tratando dados de uma API )/script.js
@@ -16,6 +16,7 @@ async function buscarEMostrarVideos() {
                         <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class= 'categoria' hidden>${video.categoria} </p>
                     </div>
                 </li>
                 `;
@@ -29,3 +30,42 @@ async function buscarEMostrarVideos() {
   }
 }
 buscarEMostrarVideos();
+
+const barradePesquisa = document.querySelector('.pesquisar__input')
+
+barradePesquisa.addEventListener('input', filtarPesquisa )
+
+function filtarPesquisa() {
+
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+  
+    videos.forEach((video) => {
+      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+  
+      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+  }
+  
+const buttonCategoria = document.querySelectorAll('.superior__item')
+
+buttonCategoria.forEach((button) => {
+  let nomeCategoria = button.getAttribute('name')
+  button.addEventListener('click', () => {
+    filtrarPorCategoria(nomeCategoria)
+  })
+})
+
+function filtrarPorCategoria (filtro) {
+  const videos = document.querySelectorAll('.videos__item')
+  videos.forEach((video) => {
+    let categoria = video.querySelector('.categoria').textContent.toLowerCase()
+    let valorFiltro = filtro.toLowerCase()
+
+    if (categoria.includes(valorFiltro) && valorFiltro != "Tudo") {
+      video.style.display = 'none'
+    } else {
+      video.style.display = 'block'
+    }
+  })
+}
