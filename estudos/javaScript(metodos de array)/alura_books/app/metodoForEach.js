f1346d30754cda5001e79eb4da0bf368840dce06
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-01-17 23:22:03 -0300

    update

--- estudos/javaScript(metodos de array)/alura_books/app/metodoForEach.js
+++ estudos/javaScript(metodos de array)/alura_books/app/metodoForEach.js
@@ -9,7 +9,7 @@ function displayBooksOnScreen(bookList) {
           ${book.titulo}
         </h2>
         <p class="livro__descricao">${book.autor}</p>
-        <p class="livro__preco" id="preco">R$${book.preco}</p>
+        <p class="livro__preco" id="preco">R$${book.preco.toFixed(2)}</p>
         <div class="tags">
           <span class="tag">${book.categoria}</span>
         </div>
